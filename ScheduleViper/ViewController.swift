6d4183a377020942a6ea5df4ccbc6f3beb3441b1
Urban <urbanovich.1@mail.ru>
2022-05-01 22:32:16 +0300

    Initial Commit

---
+++ ScheduleViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScheduleViper
+//
+//  Created by Ignat Urbanovich on 1.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
